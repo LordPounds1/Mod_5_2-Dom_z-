376201566a7c90ce7da0f5a188b39cbd84a79ff1
LordPounds1 <91436904+LordPounds1@users.noreply.github.com>
2023-10-13 13:06:27 +0600

    Add project files.

---
+++ Mod_5_2(Dom_z)/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Mod_5_2_Dom_z_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = { 1, 2, 3, 4, 5 };
+
+            try
+            {
+                for (int i = 0; i <= array.Length; i++)
+                {
+                    Console.WriteLine(array[i]);
+                }
+            }
+            catch (IndexOutOfRangeException)
+            {
+                Console.WriteLine("Выход индекса массива за пределы значений ");
+            }
+            finally
+            {
+                Console.WriteLine("Обработка массива завершена");
+            }
+        }
+    }
+}
